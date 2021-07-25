71a942f4c922df5467df864edd4e24bf1d2c4f65
Zobee <zohebboga@hotmail.com>
2021-07-25 15:00:12 -0400

    Added start screen, and moved game logic into a function. Added reset button functionality

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
 import { buildBoard, markTile, revealTile, checkWin, checkLoss, TILE_STATUSES } from "./mindweeper.js"
 
-const checkGameStatus = () => {
-  const win = checkWin(board);
-  const loss = checkLoss(board);
 
-  if (win || loss) {
-    boardElem.addEventListener('click', stopProp, {capture: true})
-    boardElem.addEventListener('contextmenu', stopProp, {capture: true})
-  }
-  if (win) msgText.innerHTML = "You Win :)"
-  if (loss) {
-    board.forEach(row => row.forEach(tile => {
-      if(tile.status === TILE_STATUSES.MARKED) markTile(tile)
-      if(tile.armed) revealTile(board, tile)
-      }))
-    msgText.innerHTML = "You Lose :("
-  }
-}
 
 const stopProp = (e) => {
   e.stopImmediatePropagation()
@@ -25,14 +9,42 @@ const stopProp = (e) => {
 const BOARD_SIZE = 10;
 const NUM_MINES = 10
 
-const board = buildBoard(BOARD_SIZE, NUM_MINES)
 const boardElem = document.querySelector('.board')
 const mineCount = document.querySelector(".mine-count")
 const resetBtn = document.querySelector(".reset")
 const msgText = document.querySelector(".subtext")
 
-board.forEach(row => row.forEach(tile => {
+const reset = () => {
+  boardElem.removeEventListener('click', stopProp, {capture: true})
+  boardElem.removeEventListener('contextmenu', stopProp, {capture: true})
+  boardElem.innerHTML=""
+  msgText.innerHTML=""
+  startGame()
+}
+
+const startGame = () => {
+  resetBtn.textContent = 'Reset'
+  const checkGameStatus = () => {
+    const win = checkWin(board);
+    const loss = checkLoss(board);
+  
+    if (win || loss) {
+      boardElem.addEventListener('click', stopProp, {capture: true})
+      boardElem.addEventListener('contextmenu', stopProp, {capture: true})
+    }
+    if (win) msgText.innerHTML = "You Win :)"
+    if (loss) {
+      board.forEach(row => row.forEach(tile => {
+        if(tile.status === TILE_STATUSES.MARKED) markTile(tile)
+        if(tile.armed) revealTile(board, tile)
+        }))
+      msgText.innerHTML = "You Lose :("
+    }
+  }
+  const board = buildBoard(BOARD_SIZE, NUM_MINES)
+  board.forEach(row => row.forEach(tile => {
   boardElem.append(tile.element)
+
   tile.element.addEventListener('click', () => {
     revealTile(board, tile)
     checkGameStatus()
@@ -46,6 +58,8 @@ board.forEach(row => row.forEach(tile => {
 boardElem.style.setProperty("--size", BOARD_SIZE)
 mineCount.textContent = NUM_MINES
 
+}
+
 resetBtn.addEventListener('click', () => {
-  window.location = '/'
-})
+  reset()
+})  
